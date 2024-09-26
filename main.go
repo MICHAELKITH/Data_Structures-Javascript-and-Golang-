4859a9eb77deeb5b68ecc0cff56af42ead5cb28d
MICHAELKITH <michaelkithinji27@gmail.com>
2024-09-26 23:06:29 +0300

    FizzBuzz challenge in Goland and Javascript

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Function to check if a number is prime
+func isPrime(num int) bool {
+	if num < 2 {
+		return false
+	}
+	for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// Function to print Fizz, Buzz, FizzBuzz, and Prime
+func fizzBuzzPrime() {
+	for i := 1; i <= 100; i++ {
+		output := ""
+
+		// Check if the number is prime
+		if isPrime(i) {
+			output += "Prime "
+		}
+
+		// Check for multiples of 3, 5, or both
+		if i%3 == 0 && i%5 == 0 {
+			output += "FizzBuzz"
+		} else if i%3 == 0 {
+			output += "Fizz"
+		} else if i%5 == 0 {
+			output += "Buzz"
+		}
+
+		// If none of the above, just print the number
+		if output == "" {
+			fmt.Println(i)
+		} else {
+			fmt.Println(output)
+		}
+	}
+}
+
+func main() {
+	// Call the function to run the program
+	fizzBuzzPrime()
+}
---
+++ main.js
@@ -0,0 +1,39 @@
+// Function to check if a number is prime
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+  }
+  return true;
+}
+
+// Function to print Fizz, Buzz, FizzBuzz, and Prime
+function fizzBuzzPrime() {
+  for (let i = 1; i <= 100; i++) {
+      let output = '';
+
+      // Check if the number is prime
+      if (isPrime(i)) {
+          output += 'Prime ';
+      }
+
+      // Check for multiples of 3, 5, or both
+      if (i % 3 === 0 && i % 5 === 0) {
+          output += 'FizzBuzz';
+      } else if (i % 3 === 0) {
+          output += 'Fizz';
+      } else if (i % 5 === 0) {
+          output += 'Buzz';
+      }
+
+      // If none of the above, just print the number
+      if (output === '') {
+          output = i;
+      }
+
+      console.log(output);
+  }
+}
+
+// Call the function to run the program
+fizzBuzzPrime();
