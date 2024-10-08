99bdb207cce1bd42cbe7cf156c768255c7a4e372
MICHAELKITH <michaelkithinji27@gmail.com>
2024-10-08 15:50:35 +0300

    DS in dart

---
+++ main.dart
@@ -0,0 +1,10 @@
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void main() {
+  print('Hello, World!');
+  var result = fibonacci(20);
+  print(result);
+}
